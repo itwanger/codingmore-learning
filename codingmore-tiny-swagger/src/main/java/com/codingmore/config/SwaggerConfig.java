fb4af33d7b6885617e50bb17d0d31916bb83fb46
itwanger <www.qing_gee@163.com>
2022-02-07 15:46:26 +0800

    Knife4j

--- codingmore-tiny-swagger/src/main/java/com/codingmore/config/SwaggerConfig.java
+++ codingmore-tiny-swagger/src/main/java/com/codingmore/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.codingmore.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,6 +11,7 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
