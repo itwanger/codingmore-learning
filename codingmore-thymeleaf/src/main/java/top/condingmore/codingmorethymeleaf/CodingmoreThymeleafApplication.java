7be7226a9fc6258a4884ee7aef0940703a2ab938
itwanger <www.qing_gee@163.com>
2022-05-09 17:12:02 +0800

    thymeleaf

---
+++ codingmore-thymeleaf/src/main/java/top/condingmore/codingmorethymeleaf/CodingmoreThymeleafApplication.java
@@ -0,0 +1,13 @@
+package top.condingmore.codingmorethymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingmoreThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingmoreThymeleafApplication.class, args);
+    }
+
+}
---
+++ codingmore-thymeleaf/src/test/java/top/condingmore/codingmorethymeleaf/CodingmoreThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package top.condingmore.codingmorethymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingmoreThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
