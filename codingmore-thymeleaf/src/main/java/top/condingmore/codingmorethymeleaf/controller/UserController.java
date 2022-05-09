e96ae0d6440fb80c85b45d4ef5c7bcf6845eba2d
itwanger <www.qing_gee@163.com>
2022-05-09 17:55:27 +0800

    控制器

---
+++ codingmore-thymeleaf/src/main/java/top/condingmore/codingmorethymeleaf/controller/UserController.java
@@ -0,0 +1,16 @@
+package top.condingmore.codingmorethymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 微信搜索「沉默王二」，回复 Java
+ *
+ * @author 沉默王二
+ * @date 5/9/22
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+    
+}
