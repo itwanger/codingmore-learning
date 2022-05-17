55ce1621c9002b49536b1ac20e4b70bab92e1206
itwanger <www.qing_gee@163.com>
2022-05-17 19:08:10 +0800

    AutoGenerator

---
+++ codingmore-autogenerator/src/main/java/top/codingmore/CodingmoreAutogeneratorApplication.java
@@ -0,0 +1,13 @@
+package top.codingmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingmoreAutogeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingmoreAutogeneratorApplication.class, args);
+    }
+
+}
