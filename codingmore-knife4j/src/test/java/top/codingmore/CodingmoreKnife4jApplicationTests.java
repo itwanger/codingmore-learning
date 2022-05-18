9ae086d989179344c036aa3fd5bebc68f6fb54ba
itwanger <www.qing_gee@163.com>
2022-05-18 21:05:07 +0800

    knife4j

---
+++ codingmore-knife4j/src/test/java/top/codingmore/CodingmoreKnife4jApplicationTests.java
@@ -0,0 +1,13 @@
+package top.codingmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingmoreKnife4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
