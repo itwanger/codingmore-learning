c25647bba713af8e0e0ab8afa132ef77de33a68d
itwanger <www.qing_gee@163.com>
2022-05-18 08:34:09 +0800

    swagger

---
+++ codingmore-swagger/src/main/java/top/codingmore/CodingmoreSwaggerApplication.java
@@ -0,0 +1,13 @@
+package top.codingmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingmoreSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingmoreSwaggerApplication.class, args);
+    }
+
+}
